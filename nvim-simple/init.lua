92eabcc6e3866e58b18227ebbb278a768346742b
Filipe Andrade <filipeyay@gmail.com>
2026-05-14 00:32:34 -0300

    quick fix on simple neovim init.lua

--- nvim-simple/init.lua
+++ nvim-simple/init.lua
@@ -1,20 +1,26 @@
+-- background
+vim.api.nvim_set_hl(0, "Normal", { bg = "#101010" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#101010" })
 
--- Mostrar números das linhas
+-- cliboard
+vim.opt.clipboard = "unnamedplus"
+
+-- show line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Indentação básica
+-- identation
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
--- Syntax highlight
+-- syntax highlight
 vim.cmd("syntax on")
 
--- True color
+-- true color
 vim.opt.termguicolors = true
 
--- Auto fechar pares
+-- auto close
 local autopairs = {
     ["("] = ")",
     ["["] = "]",
