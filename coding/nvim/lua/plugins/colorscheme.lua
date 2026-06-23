3bde45d27496a4214864931442a8ed0570028117
Filipe Andrade <filipeyay@gmail.com>
2026-06-23 02:56:25 -0300

    better folder structure

---
+++ coding/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "oskarnurm/koda.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      styles = {
+        functions = { italic = false },
+        keywords = { italic = false },
+        comments = { italic = false },
+        strings = { italic = false },
+        constants = { italic = false },
+      },
+    },
+    config = function() end,
+  },
+}
